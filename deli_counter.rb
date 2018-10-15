093fb237d6d61d493db019488bebbe9dbc21dccf
Matthew Migliore <mmigliore311@gmail.com>
2018-10-15 15:21:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
   if katz_deli.count == 0 
     puts "The line is currently empty."
   else 
-    puts "The line is currently: #{current_line.each do |customer| puts " #{customer}"}."
+    puts "The line is currently: #{current_line.each do |customer| puts ' #{customer}'}."
   end
 end
 
